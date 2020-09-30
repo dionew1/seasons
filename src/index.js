e77ec060b87d89fbb71396fda5bc499131e741e4
Dione Wilson <dionew1@gmail.com>
2020-09-29 18:00:02 -0600

    Add call to get window's location

--- src/index.js
+++ src/index.js
@@ -2,7 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 const App = () => {
-  return <div>Hello There!</div>;
+  window.navigator.geolocation.getCurrentPosition(
+    (position) => console.log(position),
+    (error) => console.error(error)
+  );
+  return <div>Hello there!</div>;
 };
 
 ReactDOM.render(<App />, document.querySelector("#root"));
+
+// developer.mozilla.org/en-US/docs/Web/API/Geolocation_API: Used to get the users physical location
