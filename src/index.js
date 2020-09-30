77c7dc85eca2a65f89eaf5b359d5b1d9f2d557e1
Dione Wilson <dionew1@gmail.com>
2020-09-30 11:57:22 -0600

    Add setState to update state

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+// developer.mozilla.org/en-US/docs/Web/API/Geolocation_API: Used to get the users physical location
 import React from "react";
 import ReactDOM from "react-dom";
 
@@ -5,17 +6,21 @@ class App extends React.Component {
   constructor(props) {
     super(props);
 
+    // direct assignment to state only to initialize state
     this.state = { lat: null };
-  }
-  render() {
+
     window.navigator.geolocation.getCurrentPosition(
-      (position) => console.log(position),
+      (position) => {
+        // setState function is extended from React.Component
+        // setState is called to UPDATE state
+        this.setState({ lat: position.coords.latitude });
+      },
       (error) => console.error(error)
     );
-    return <div>Latitude: </div>;
+  }
+  render() {
+    return <div>Latitude: {this.state.lat}</div>;
   }
 }
 
 ReactDOM.render(<App />, document.querySelector("#root"));
-
-// developer.mozilla.org/en-US/docs/Web/API/Geolocation_API: Used to get the users physical location
